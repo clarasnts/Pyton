5a48c282aaa3d0b876724fb4fcf2bf74a425cdff
Ana Clara Santos <anaclara735901@gmail.com>
2024-08-23 18:50:49 -0300

    Add files via upload

---
+++ 03.aula02.py
@@ -0,0 +1,35 @@
+# comentários de uma linha
+# tipagem dinâmica:
+
+# teste="olá"
+# print(type(teste))
+# teste=30
+# print(type(teste))
+# teste=2.4
+# print(type(teste))
+
+# nome=input("Digite seu nome: ")
+# print("Olá", nome)
+# idade=input("Digite sua idade: ")
+# print("Olá", nome,"você tem", idade, "anos.")
+
+# Tipos De Dados:
+# int: números inteiros
+# float: números reais
+# str: string cadeia de caracteres, ultilizando aspas
+# boolean: tipo lógico true ou false
+
+# valorA= int(input("Digite o valor de A: "))
+# valorB= int(input("Digite o valor de B: "))
+# print(valorA+valorB)
+# print(type(valorA))
+
+#Operadores aritméticos
+# x=20
+# print(x+4) #Adição
+# print(x-7) #Subtração
+# print(x*2) #Multipricação
+# print(x/2) #Divisão
+# print(x//6) #Divisão inteira
+# print(x**4) #Potenciação
+# print(x%2) #Resto da divisão
