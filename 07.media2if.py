2588d2135875c24b36ad4b0548096f191005173d
Ana Clara Santos <anaclara735901@gmail.com>
2024-08-23 18:15:25 -0300

    Add files via upload

---
+++ 07.media2if.py
@@ -0,0 +1,17 @@
+print("Olá! Vamos ver qual será sua media.")
+
+nota1=float(input("Digite o valor da sua primeira nota: "))
+nota2=float(input("Digite o valor da sua segunda nota: "))
+nota3=float(input("Digite o valor da sua terceira nota: "))
+nota4=float(input("Digite o valor da sua quarta nota: "))
+
+media=(nota1+nota2+nota3+nota4)/4
+
+print("Sua média é", media)
+
+if(media>=80):
+    print("Aluno aprovado!")
+elif(media>=60):
+    print("Aluno de recuperação!")
+else:
+    print("Aluno reprovado!")
