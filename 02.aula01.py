ae049f2d5ec4c01d2fdc7b841111f81dcdb7ad8f
Ana Clara Santos <anaclara735901@gmail.com>
2024-08-23 11:46:16 -0300

    Add files via upload

---
+++ 02.aula01.py
@@ -0,0 +1,10 @@
+name = "Clara"
+idade = 15
+
+print ("Olá", name, "a sua idade é", idade)
+
+name = "Clara"
+idade = 15
+altura = 1.53
+
+print ("Olá", name, "a sua idade é", idade, "a sua altura é", altura)
