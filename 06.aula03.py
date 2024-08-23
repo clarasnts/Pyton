be0f41a70c4e84e7d43309e70728ac50384cfa1c
Clara Santos <anaclara735901@gmail.com>
2024-08-23 18:59:49 -0300

    Add files via upload

---
+++ 06.aula03.py
@@ -0,0 +1,8 @@
+condicao1=False
+
+if (condicao1):
+    print("A condição 1 é verdadeira")
+    print("Outra linha dentro do condição 1")
+else:
+    print("A condição 1 é falsa")
+print("fora do if")
